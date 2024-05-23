e8b79bda8577612d3cf25ab963e9ceb2c1ecc50f
Dimatron <diman2421@mail.ru>
2024-05-23 14:48:04 +0300

    new commit

--- tests/settings.go
+++
@@ -1,7 +0,0 @@
-package tests
-
-var Port = 7540
-var DBFile = "../scheduler.db"
-var FullNextDate = false
-var Search = true
-var Token = `yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ItCU0LzQuNGC0YDQuNC5INCbIiwiaWF0IjoxNTE2MjM5MDIyfQ.15IT4MVKtI3MiGiufEnSh8kvNVBssyiaXMjUK2fWxew`
